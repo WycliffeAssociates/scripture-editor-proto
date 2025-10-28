fb611bcbf42e449d5a87c77b710ed50457d06455
Joe <jsarabia1247@gmail.com>
2025-10-28 17:03:15 -0400

    cleanup

--- src/app/routes/$project.tsx
+++ src/app/routes/$project.tsx
@@ -19,7 +19,7 @@ export const Route = createFileRoute("/$project")({
         // start here would prefer to wrap into a single abstraction
         const {directoryProvider, projectRepository} = context;
         const { project } = params;
-        const parsedFiles = await projectParamToParsedFiles2(
+        const parsedFiles = await projectParamToParsedFiles(
             projectRepository,
             project
         );
@@ -43,65 +43,6 @@ function RouteComponent() {
 
 // todo: abstract off somewhere else and separate a littler better
 export async function projectParamToParsedFiles(
-    directoryProvider: IDirectoryProvider,
-    project: string | undefined
-) {
-    console.time("getFileHandle");
-    if (!project) return [];
-    const thisDir = await directoryProvider.getDirectoryHandle(project);
-    const manifestHandle = (await thisDir.getFileHandle(
-        "manifest.yaml"
-    )) as FileSystemFileHandle;
-    const manifestFile = await manifestHandle.getFile();
-    const manifestText = await manifestFile.text();
-    const parsedManifest = parse(manifestText);
-    const language = parsedManifest?.dublin_core?.language;
-    const files = await directoryProvider.getDirectoryHandle(project);
-    const entry: Array<{
-        path: string;
-        name: string;
-        file: File;
-        text: string;
-    }> = [];
-    for await (const [name, handle] of files.entries()) {
-        if (name.endsWith(".usfm") && handle.kind === "file") {
-            const h = handle as FileSystemFileHandle;
-            const file = await h.getFile();
-            const text = await file.text();
-            // @ts-ignore TODO: GENERIC INTERFACE FOR WEB + TAURI
-            entry.push({path: handle.path, name, file, text});
-        }
-    }
-    console.timeEnd("getFileHandle");
-    console.time("parse");
-    // end here would prefer to wrap into a single abstraction
-    // Next function call as parsing and going to lexicla state is separate is fine
-    const parsed: ParsedFile[] = sorted.map((file, i) => {
-        const parsed = parseUSFMfile(file.text);
-        return {
-            title: file.name,
-            bookCode: getBookSlug(file.name),
-            nextBookId:
-                i === sorted.length - 1 ? null : getBookSlug(sorted[i + 1]?.name),
-            prevBookId: i === 0 ? null : getBookSlug(sorted[i - 1]?.name),
-            path: file.path,
-            chapters: Object.entries(parsed).map(([chapter, tokens]) => ({
-                lexicalState: parsedUsfmTokensToJsonLexicalNode(
-                    tokens,
-                    language.direction
-                ),
-                chapNumber: Number(chapter),
-                dirty: false,
-            })),
-        };
-    });
-    console.timeEnd("parse");
-    console.timeEnd("total time");
-    return parsed;
-}
-
-// todo: abstract off somewhere else and separate a littler better
-export async function projectParamToParsedFiles2(
     projectRepository: IProjectRepository,
     project: string | undefined
 ) {
