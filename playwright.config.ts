49bf1baf84fb407ef8a225ab8105c612a623745f
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2025-12-10 16:20:42 -0600

    adjust tests in ci config

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,9 +75,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: process.env.CI
+    ? {
+        command: "pnpm run preview",
+        url: "http://localhost:4173",
+        reuseExistingServer: !process.env.CI,
+      }
+    : undefined,
 });
